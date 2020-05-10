02482d550c234a4061a7dff85d09025f165ec20d
yaeldina <yaeldinaa@gmail.com>
2020-05-10 16:17:59 -0700

    added nasa classes

---
+++ src/main/java/aufrichtig/nasa/neo/NeoFeed.java
@@ -0,0 +1,35 @@
+package aufrichtig.nasa.neo;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class NeoFeed {
+
+    @SerializedName("near_earth_objects")
+    HashMap<String, List<NearEarthObject>> nearEarthObjects;
+
+    class NearEarthObject {
+        String id;
+        String name;
+        @SerializedName("nasa_jpl_url")
+        String nasaJplUrl;
+        @SerializedName("is_potentially_hazardous_asteroid")
+        boolean hazardous;
+        @SerializedName("close_approach_data")
+        List<CloseApproachData> closeApproachData;
+    }
+
+    class CloseApproachData {
+        @SerializedName("close_approach_date")
+        String closeApproachDate;
+        @SerializedName("miss_distance")
+        MissDistance missDistance;
+    }
+
+    class MissDistance {
+        double lunar;
+    }
+
+}
